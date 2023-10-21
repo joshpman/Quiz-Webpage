a5d1e7617690d743422e6254c8dc4a47a5c746b2
Josh Pearlman <82927774+joshpman@users.noreply.github.com>
2023-10-20 23:55:34 -0400

    Spacing issue

--- quiz-webpage/src/Categories.jsx
+++ quiz-webpage/src/Categories.jsx
@@ -1,6 +1,6 @@
 const Categories = ({type})=>{
     return(
-        <option  value={type.id}>{type.name}</option>
+        <option value={type.id}>{type.name}</option>
     )
 }
 export default Categories;
